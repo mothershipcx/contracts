da0e830d267c68805d163c21c3fdbb50d1f9fecc
Anton Egorov <anton.egoroff@gmail.com>
2017-06-24 00:50:31 +0300

    SIT and MSP are based MiniMe now

--- contracts/SIT.sol
+++ contracts/SIT.sol
@@ -1,17 +1,21 @@
 pragma solidity ^0.4.11;
 
-import "zeppelin-solidity/contracts/ownership/HasNoEther.sol";
-import "./MetaToken.sol";
+import "./MiniMeToken.sol";
 
-/**
- * @title Strategic Investors Token
- */
-contract SIT is HasNoEther, MetaToken {
-  function SIT()
-    MetaToken(
-              "Strategic Investors Token",
-              "SIT",
-              18,
-              40000000
-    ) {}
+/*
+  Copyright 2017, Anton Egorov (Mothership)
+*/
+
+contract SIT is MiniMeToken {
+
+  function SIT(address _tokenFactory)
+    MiniMeToken(
+                _tokenFactory,
+                0x0,                         // no parent token
+                0,                           // no snapshot block number from parent
+                "Strategic Investors Token", // Token name
+                18,                          // Decimals
+                "SIT",                       // Symbol
+                false                        // Enable transfers
+                ) {}
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,38 @@
+const MiniMeTokenFactory = artifacts.require('MiniMeTokenFactory')
 const SIT = artifacts.require('SIT')
 const MSP = artifacts.require('MSP')
 
-module.exports = function(deployer) {
-  deployer.deploy(SIT)
-  deployer.deploy(MSP)
+const addressSIT = ''
+
+module.exports = async function(deployer) {
+  // MiniMeTokenFactory send
+  const miniMeTokenFactoryFuture = MiniMeTokenFactory.new()
+
+  // MiniMeTokenFactory wait
+  const miniMeTokenFactory = await miniMeTokenFactoryFuture
+  console.log('MiniMeTokenFactory: ' + miniMeTokenFactory.address)
+  console.log()
+
+  // SIT send
+  let sitFuture
+  if (addressSIT.length === 0) {
+    // Testnet
+    sitFuture = SIT.new(miniMeTokenFactory.address)
+  } else {
+    sitFuture = SIT.at(addressSIT)
+  }
+  // MSP send
+  const mspFuture = MSP.new(miniMeTokenFactory.address)
+
+  // SIT wait
+  const sit = await sitFuture
+  console.log('SIT: ' + sit.address)
+  // MSP wait
+  const msp = await mspFuture
+  console.log('MSP: ' + msp.address)
+  console.log()
+
+  // MSP initialize checkpoints for 0th TX gas savings
+  await msp.generateTokens('0x0', 1)
+  await msp.destroyTokens('0x0', 1)
 }
