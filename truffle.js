72a93f597dca438cdf9a6675769e6e5769ceb63d
Anton Egorov <anton.egoroff@gmail.com>
2017-06-24 17:04:33 +0300

    ropsten and kovan networks

--- truffle.js
+++ truffle.js
@@ -1,9 +1,39 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const mnemonic =
+  process.env.TEST_MNEMONIC ||
+  'mothership mnemonic mothership mnemonic mothership mnemonic mothership mnemonic mothership mnemonic mothership mnemonic'
+const providerRopsten = new HDWalletProvider(
+  mnemonic,
+  'https://ropsten.infura.io/',
+  0,
+)
+const providerKovan = new HDWalletProvider(
+  mnemonic,
+  'https://kovan.infura.io',
+  0,
+)
+
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      network_id: 15,
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
-};
+      gas: 4000000,
+      gasPrice: 20e9,
+    },
+    ropsten: {
+      network_id: 3,
+      provider: providerRopsten,
+      gas: 4000000,
+      gasPrice: 20e9,
+    },
+    kovan: {
+      network_id: 42,
+      provider: providerKovan,
+      gas: 4000000,
+      gasPrice: 20e9,
+    },
+  },
+}
