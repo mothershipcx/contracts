1af204ea2c0028a226825b79226d99acb65cc815
Anton Egorov <anton.egoroff@gmail.com>
2017-06-24 17:14:23 +0300

    fix token name

--- contracts/SIT.sol
+++ contracts/SIT.sol
@@ -11,11 +11,11 @@ contract SIT is MiniMeToken {
   function SIT(address _tokenFactory)
     MiniMeToken(
                 _tokenFactory,
-                0x0,                         // no parent token
-                0,                           // no snapshot block number from parent
-                "Strategic Investors Token", // Token name
-                18,                          // Decimals
-                "SIT",                       // Symbol
-                false                        // Enable transfers
+                0x0,                        // no parent token
+                0,                          // no snapshot block number from parent
+                "Strategic Investor Token", // Token name
+                18,                         // Decimals
+                "SIT",                      // Symbol
+                false                       // Enable transfers
                 ) {}
 }
