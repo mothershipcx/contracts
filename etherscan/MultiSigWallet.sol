2b0e846945c60c2bfe97b46779861873f34238c2
Klaus Hott Vidal <klahott@gmail.com>
2017-07-05 18:40:16 +1200

    standard licence

--- etherscan/MultiSigWallet.sol
+++ etherscan/MultiSigWallet.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.4.11;
 
+/*
+  Copyright 2017, Stefan George (Consensys)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 /// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
 /// @author Stefan George - <stefan.george@consensys.net>
--- etherscan/SIT.sol
+++ etherscan/SIT.sol
@@ -5,6 +5,19 @@ pragma solidity ^0.4.11;
   Copyright 2017, Klaus Hott (BlockchainLabs.nz)
   Copyright 2017, Jordi Baylina (Giveth)
 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
   Based on MineMeToken.sol from https://github.com/Giveth/minime
   Original contract from https://github.com/status-im/status-network-token/blob/master/contracts/MiniMeToken.sol
 */
